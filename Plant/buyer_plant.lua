759a7a1cb031b4e9e0a2775a9aa7aeb38ec946fa
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-06-14 10:40:41 +0700

    Update buyer_plant.lua

--- Plant/buyer_plant.lua
+++ Plant/buyer_plant.lua
@@ -1,3 +1,4 @@
 ListPembeli = {
-    {iddc = "422670528483033088", nick = "186MalesBgt"}
+    {iddc = "422670528483033088", nick = "186MalesBgt"},
+    {iddc = "711698231855415397", nick = "johanken"}
 }
