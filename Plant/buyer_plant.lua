22f53e1ce29ea3edc352203e47384aadbf25f713
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-07-01 17:42:28 +0700

    Update buyer_plant.lua

--- Plant/buyer_plant.lua
+++ Plant/buyer_plant.lua
@@ -30,5 +30,6 @@ ListPembeli = {
     {iddc = "381719055704260608", nick = "Axillaris"},
     {iddc = "392179093803761665", nick = "ObssesedWithSam"},
     {iddc = "549572465202823168", nick = "MORGANLER"},
-    {iddc = "720420548533813308", nick = "Samuyok"}
+    {iddc = "720420548533813308", nick = "Samuyok"},
+    {iddc = "958963119999299594", nick = "Greyiscoming"}
 }
