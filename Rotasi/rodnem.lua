f6116537fdf24b1a326e16fa211de2db92195796
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-07-10 14:09:52 +0700

    Update rodnem.lua

--- Rotasi/rodnem.lua
+++ Rotasi/rodnem.lua
@@ -212,5 +212,5 @@ ListPembeli = {
     {iddc = "930845935351791658", nick = "FaryaRiz"},
     {iddc = "1006432442632122370", nick = "vonzyyyyyy02"},
     {iddc = "405648492145541150", nick = "ilhamexe1"},
-    {iddc = "710602113398472747", nick = "fref"}
+    {iddc = "710602113398472747", nick = "hudajp"}
 }
